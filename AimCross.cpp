efe27ffe7e30c66f545d212d427d0672538f30cf
Zer0Day <dankraus111@gmail.com>
2025-06-16 23:53:21 +0200

    Create AimCross.cpp

---
+++ AimCross.cpp
@@ -0,0 +1,22 @@
+#include "FPSHUD.h"
+#include "Engine/Canvas.h"
+#include "Engine/Texture2D.h"
+
+void AFPSHUD::DrawHUD()
+{
+    Super::DrawHUD();
+
+    if (CrosshairTexture)
+    {
+        FVector2D Center(Canvas->ClipX * 0.5f, Canvas->ClipY * 0.5f);
+
+        FVector2D CrossHairDrawPosition(
+            Center.X - (CrosshairTexture->GetSizeX() * 0.5f),
+            Center.Y - (CrosshairTexture->GetSizeY() * 0.5f)
+        );
+
+        FCanvasTileItem TileItem(CrossHairDrawPosition, CrosshairTexture->Resource, FLinearColor::White);
+        TileItem.BlendMode = SE_BLEND_Translucent;
+        Canvas->DrawItem(TileItem);
+    }
+}

02c8b99f45c602935e7721dd160d827ac10b9154
Zer0Day <dankraus111@gmail.com>
2025-06-16 23:56:59 +0200

    Update Character.h

--- Character.h
+++ Character.h
@@ -1,35 +1,40 @@
 #pragma once
-     
+
+#include "CoreMinimal.h"
 #include "GameFramework/Character.h"
 #include "Camera/CameraComponent.h"
 #include "Components/CapsuleComponent.h"
+#include "FPSProjectile.h"
 #include "FPSCharacter.generated.h"
-     
+
 UCLASS()
 class FPSPROJECT_API AFPSCharacter : public ACharacter
 {
     GENERATED_BODY()
- 
+
 public:
     AFPSCharacter();
-     
-protected:          
+
+protected:
     virtual void BeginPlay() override;
-     
-public: 
-    virtual void Tick( float DeltaTime ) override;
-     
+
+    UPROPERTY(EditAnywhere, Category = Projectile)
+    TSubclassOf<class AFPSProjectile> ProjectileClass;
+
+public:
+    virtual void Tick(float DeltaTime) override;
+
     virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
-     
+
     UFUNCTION()
     void MoveForward(float Value);
-     
+
     UFUNCTION()
     void MoveRight(float Value);
-     
+
     UFUNCTION()
     void StartJump();
-     
+
     UFUNCTION()
     void StopJump();
 
@@ -38,4 +43,10 @@ public:
 
     UPROPERTY(VisibleDefaultsOnly, Category = Mesh)
     USkeletalMeshComponent* FPSMesh;
+
+    UFUNCTION()
+    void Fire();
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Gameplay)
+    FVector MuzzleOffset;
 };

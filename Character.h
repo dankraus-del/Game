aabdc7c6c1094744c10f071b147acf8e0941d3fb
Zer0Day <dankraus111@gmail.com>
2025-06-17 00:00:16 +0200

    Update Character.h

--- Character.h
+++ Character.h
@@ -4,22 +4,22 @@
 #include "GameFramework/Character.h"
 #include "Camera/CameraComponent.h"
 #include "Components/CapsuleComponent.h"
-#include "FPSProjectile.h"
-#include "FPSCharacter.generated.h"
+#include "Projectile.h"
+#include "Character.generated.h"
 
 UCLASS()
-class FPSPROJECT_API AFPSCharacter : public ACharacter
+class Game_API ACharacter : public ACharacter
 {
     GENERATED_BODY()
 
 public:
-    AFPSCharacter();
+    ACharacter();
 
 protected:
     virtual void BeginPlay() override;
 
     UPROPERTY(EditAnywhere, Category = Projectile)
-    TSubclassOf<class AFPSProjectile> ProjectileClass;
+    TSubclassOf<class AProjectile> ProjectileClass;
 
 public:
     virtual void Tick(float DeltaTime) override;

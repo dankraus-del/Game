6a5c1c54952cd92f28d264c6b1a5d4cfd087fe9a
Zer0Day <dankraus111@gmail.com>
2025-06-17 00:13:37 +0200

    Create Projectile.cpp

---
+++ Projectile.cpp
@@ -0,0 +1,86 @@
+#include "Projectile.h"
+
+// Sets default values
+AProjectile::AProjectile()
+{
+    PrimaryActorTick.bCanEverTick = true;
+
+    if (!RootComponent)
+    {
+        RootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("ProjectileSceneComponent"));
+    }
+
+    if (!CollisionComponent)
+    {
+        CollisionComponent = CreateDefaultSubobject<USphereComponent>(TEXT("SphereComponent"));
+        CollisionComponent->BodyInstance.SetCollisionProfileName(TEXT("Projectile"));
+        CollisionComponent->OnComponentHit.AddDynamic(this, &AFPSProjectile::OnHit);
+        CollisionComponent->InitSphereRadius(15.0f);
+        RootComponent = CollisionComponent;
+    }
+
+    if (!ProjectileMovementComponent)
+    {
+        ProjectileMovementComponent = CreateDefaultSubobject<UProjectileMovementComponent>(TEXT("ProjectileMovementComponent"));
+        ProjectileMovementComponent->SetUpdatedComponent(CollisionComponent);
+        ProjectileMovementComponent->InitialSpeed = 3000.0f;
+        ProjectileMovementComponent->MaxSpeed = 3000.0f;
+        ProjectileMovementComponent->bRotationFollowsVelocity = true;
+        ProjectileMovementComponent->bShouldBounce = true;
+        ProjectileMovementComponent->Bounciness = 0.3f;
+        ProjectileMovementComponent->ProjectileGravityScale = 0.0f;
+    }
+
+    if (!ProjectileMeshComponent)
+    {
+        ProjectileMeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("ProjectileMeshComponent"));
+
+        static ConstructorHelpers::FObjectFinder<UStaticMesh> Mesh(TEXT("'/Game/Sphere.Sphere'"));
+        if (Mesh.Succeeded())
+        {
+            ProjectileMeshComponent->SetStaticMesh(Mesh.Object);
+        }
+
+        static ConstructorHelpers::FObjectFinder<UMaterial> Material(TEXT("'/Game/SphereMaterial.SphereMaterial'"));
+        if (Material.Succeeded())
+        {
+            ProjectileMaterialInstance = UMaterialInstanceDynamic::Create(Material.Object, ProjectileMeshComponent);
+        }
+
+        ProjectileMeshComponent->SetMaterial(0, ProjectileMaterialInstance);
+        ProjectileMeshComponent->SetRelativeScale3D(FVector(0.09f, 0.09f, 0.09f));
+        ProjectileMeshComponent->SetupAttachment(RootComponent);
+    }
+
+    InitialLifeSpan = 3.0f;
+}
+
+void AProjectile::BeginPlay()
+{
+    Super::BeginPlay();
+}
+
+void ASProjectile::Tick(float DeltaTime)
+{
+    Super::Tick(DeltaTime);
+}
+
+void AProjectile::FireInDirection(const FVector& ShootDirection)
+{
+    ProjectileMovementComponent->Velocity = ShootDirection * ProjectileMovementComponent->InitialSpeed;
+}
+
+void AProjectile::OnHit(
+    UPrimitiveComponent* HitComponent,
+    AActor* OtherActor,
+    UPrimitiveComponent* OtherComponent,
+    FVector NormalImpulse,
+    const FHitResult& Hit)
+{
+    if (OtherActor != nullptr && OtherActor != this && OtherComponent != nullptr && OtherComponent->IsSimulatingPhysics())
+    {
+        OtherComponent->AddImpulseAtLocation(ProjectileMovementComponent->Velocity * 100.0f, Hit.ImpactPoint);
+    }
+
+    Destroy();
+}
